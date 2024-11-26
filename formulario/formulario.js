8f570e157a4aad817f992f230978d87bd6c0183d
CristhianKindermann <cristhianrodriques@gmail.com>
2024-11-26 11:13:52 -0300

    Projeto

---
+++ formulario/formulario.js
@@ -0,0 +1,4 @@
+document.getElementById('reservaForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita o envio real do formulário
+    window.alert('Solicitado com sucesso');
+});
